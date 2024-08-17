5cc33cd9f279c33ccd8dff09e5d2152afced593d
celal uğur kocan <celal_53_kocan@hotmail.com>
2024-08-17 11:47:17 +0300

    ..

--- c#/newproject/Program.cs
+++ c#/newproject/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 10; // Fibonacci dizisinin eleman sayısı
+        Console.WriteLine("Fibonacci Dizisi:");
+
+        int[] fib = new int[n];
+        
+        // İlk iki sayıyı belirle
+        if (n > 0) fib[0] = 0;
+        if (n > 1) fib[1] = 1;
+        
+        // Fibonacci dizisini oluştur
+        for (int i = 2; i < n; i++)
+        {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        
+        // Diziyi ekrana yazdır
+        foreach (int num in fib)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
