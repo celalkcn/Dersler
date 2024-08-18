980eeb1ea860114278ad7077c03c4cf227382ed9
celal uğur kocan <celal_53_kocan@hotmail.com>
2024-08-18 10:13:58 +0300

    a

---
+++ c#/yeniproje/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+       int degiskenAdi=56;
+       int degiskenAddi2=44;
+       System.Console.WriteLine("+iki değiskenin toplamı "+ (degiskenAdi+degiskenAddi2));//cw yaz
+       // vs code ctrl ö
+       // ctrl+kc  yourmları kaldırma crtl ku
+
+
+    }
+}
