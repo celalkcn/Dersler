b96fa509b60be9b8991d5ce0cc437ed538331b47
celal uğur kocan <celal_53_kocan@hotmail.com>
2024-08-24 10:17:55 +0300

    .

--- c#/yeniproje/Program.cs
+++ c#/yeniproje/Program.cs
@@ -1,17 +1,53 @@
 ﻿
 using System;
 using System.Net;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main()
     {
-       int degiskenAdi=56;
-       int degiskenAddi2=44;
-       System.Console.WriteLine("+iki değiskenin toplamı "+ (degiskenAdi+degiskenAddi2));//cw yaz
-       // vs code ctrl ö
-       // ctrl+kc  yourmları kaldırma crtl ku
+        //     int degiskenAdi = 56;
+        //     int degiskenAddi2 = 44;
+        //     System.Console.WriteLine("+iki değiskenin toplamı " + (degiskenAdi + degiskenAddi2));//cw yaz
 
 
+        //     #region TmSyi 
+        //     // vs code ctrl ö
+        //     // ctrl+kc  yourmları kaldırma crtl ku
+        //     // c# da yazılan ayrıca belirtilmedilçe int tipinde kabul edilir
+
+        //     #endregion
+        //     byte studentPoint = 15;
+        // System.Console.WriteLine(studentPoint);
+
+        //     decimal mas = 0.5m;
+        //     #region  diğer tipler 
+
+        //                 bool varMi = true;
+        //     char ceva = 'a';
+        //     //tek veri tutar/ type vır tutugu alan misali
+        //     // düzenlemek icin format decıment 
+        //     #endregion
+
+        // 1. ödev
+        // int sayi1 = 45;
+        // int sayi2 = 31;
+        // int toplam = sayi1+sayi2;
+        // System.Console.WriteLine(toplam);    
+
+        // 2. ödev
+
+        // double sayi1 = 10.2;
+        // double sayininKaresi = Math.Pow(sayi1,2);
+        // System.Console.WriteLine("sayinin karesi "+sayininKaresi);
+
+        // 3. ödev
+        // string adSoyad= "celal uğur koçan";
+        // int yas= 25;
+        // System.Console.WriteLine("Benim adım  "+adSoyad+" ve yaşım "+yas);
+
+        // 4. ödev
+        
     }
 }
---
+++ proje_4/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace proje_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
