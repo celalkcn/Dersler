8882615f932d8925749ae9fee1873c20846fdcc4
celal uğur kocan <celal_53_kocan@hotmail.com>
2024-08-24 14:53:18 +0300

    a

--- c#/Project6/Program.cs
+++ c#/Project6/Program.cs
@@ -28,23 +28,29 @@ class Program
         #endregion
         #region nişantası uni
 
-        string text1 = "Nişantaşı";
-        string text2 = "üniversitesi";
-        string loc1 = "bayrampaşa";
-        string currentLoc = "sarıyer";
-        string date1 = "09/09/2009";
-        string date2 = "05/05/2018";
-        // bu değişkenleri kullanarak nışantaşı üniversitesi , 09.09.2009 tarihnde istanbul'un bayrampaşa lçesinde faaliyete başladoı
-        //ardından 05052018 tarihinde yine istanbulun sarıyer ilçesnde kurulan tech campus e taşındı
-        string icerik = date1 + " tarihinde istanbul'un bayrampaşa ilçesinde faaliyete başladı ardından " + date2 + "tarihinde yine istanbulun" + currentLoc + "ilcesinde kurulan tech campus e tasındı";
-        string resultMessage = $"{text1} {text2} {date1} tarihinde istanbul un {loc1} ilçesinde faaliyete başladı ardından {date2} tarihinde yine istanbulun {currentLoc} ilçesinde kurulan ech camus e tasındı";
-        System.Console.WriteLine(resultMessage);
+        // string text1 = "Nişantaşı";
+        // string text2 = "üniversitesi";
+        // string loc1 = "bayrampaşa";
+        // string currentLoc = "sarıyer";
+        // string date1 = "09/09/2009";
+        // string date2 = "05/05/2018";
+        // // bu değişkenleri kullanarak nışantaşı üniversitesi , 09.09.2009 tarihnde istanbul'un bayrampaşa lçesinde faaliyete başladoı
+        // //ardından 05052018 tarihinde yine istanbulun sarıyer ilçesnde kurulan tech campus e taşındı
+        // string icerik = date1 + " tarihinde istanbul'un bayrampaşa ilçesinde faaliyete başladı ardından " + date2 + "tarihinde yine istanbulun" + currentLoc + "ilcesinde kurulan tech campus e tasındı";
+        // string resultMessage = $"{text1} {text2} {date1} tarihinde istanbul un {loc1} ilçesinde faaliyete başladı ardından {date2} tarihinde yine istanbulun {currentLoc} ilçesinde kurulan ech camus e tasındı";
+        // System.Console.WriteLine(resultMessage);
 
 
 
 
         #endregion
 
+        string fullName = "halil umut MELER";
+        string findText = "umut";
+        bool response =fullName.Contains(findText);
+        System.Console.WriteLine(response);
+        
+        
 
 
 
