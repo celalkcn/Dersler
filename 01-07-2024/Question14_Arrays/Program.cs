f9ea535ca89ed19b544e94b9cd20de43c6f0d61c
celal uğur kocan <celal_53_kocan@hotmail.com>
2024-09-08 12:10:24 +0300

    i

---
+++ 01-07-2024/Question14_Arrays/Program.cs
@@ -0,0 +1,239 @@
+﻿using System.Globalization;
+using System.Reflection.Emit;
+using System.Security;
+
+namespace Question14_Arrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // int[] numbers;
+        // numbers = new int[5];
+
+        // numbers[0] = 45;
+        // numbers[1] = 25;
+        // numbers[2] = 15;
+        // numbers[3] = 35;
+        // numbers[4] = 105;
+
+        // for (int i= 0 ; i<5; i++)
+        //  {
+        //     System.Console.WriteLine($"Dizinin{i+1}.elemanı: {numbers[i]}");
+
+        //  }
+
+        // int[] numbers = {312,3123,312312,31121};
+        //     for (int i= 0 ; i<4; i++)
+        //      {
+        //         System.Console.WriteLine($"Dizinin{i+1}.elemanı: {numbers[i]}");
+
+        //      }
+
+        //  string [] studentNames= {
+        //     "ali",
+        //     "mehmet",
+        //     "ayşe",
+        //     "faruk",
+        //     "kerem"
+        //  };
+
+        //  for (int i = 0; i < studentNames.Length; i++)
+        //  {
+        //     studentNames[i]+= " Canoğlu";
+
+        //  }
+        //  System.Console.WriteLine();
+        //     System.Console.WriteLine("Değişmiş Hali: ");
+        // for (int i = 0; i < studentNames.Length; i++)
+        // {
+        //     System.Console.WriteLine(studentNames[i]);;
+
+        // }
+        // string[] studentNames = {
+        //     "ali",
+        //     "mehmet",
+        //     "ayşe",
+        //     "faruk",
+        //     "kerem"
+        //  };
+
+        //  foreach (string student in studentNames)
+        //  {
+        //     System.Console.WriteLine(student + " Canoğlu");
+        //      } //performans düşüklüğü yapar 
+
+        //      int[] price={40,50,60,70,80};
+
+        //      // prices dizisinin içindeki fiyatlara %10 zam yap
+
+        //      foreach (int p in price)
+        //      {
+        //         System.Console.WriteLine(p);
+        //      }
+        //      System.Console.WriteLine("zamsız ürün fiyatı: ");
+        //      System.Console.WriteLine("-------------------");
+
+
+        //      for (int i = 0; i < price.Length; i++)
+        //      {
+        //        price[i]=(int)(price[i]*1.1);
+
+        //      }
+        // foreach (int p in price)
+        // {
+        //     System.Console.WriteLine(p);
+        // }
+        // System.Console.WriteLine("zamlı ürün fiyatı: ");
+        // System.Console.WriteLine("-------------------");
+
+        // int a = 500;
+        // int b = 50;
+        // int c = 450;
+
+        // int max = int.MinValue;
+        // if (a > max)
+        // {
+        //     max = a;
+        // }
+
+        // if (b > max)
+        // {
+        //     max = b;
+        // }
+        // if (c>max)
+        // {
+        //     max=c;
+        // }
+        // System.Console.Write(max);
+
+        // 20 ELEMANLI İNT DİZİSİNE 1-100 arasında RASTGELE DEĞERLER ATAYALIM BU DİZİDEKİ EN BÜYÜK VE EN KÜÇÜK DEĞERLERİ YAZDIRALIM.
+
+        //dizimizi oluşturup içine rastgele değerler atıyoruz.
+
+        // Random rnd = new Random();
+        // int[] numbers = new int[20];
+
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     numbers[i] = rnd.Next(1, 101);
+        //     System.Console.WriteLine(numbers[i]);
+
+        // }
+        // // en büyük ve en küçük değeri buluyoruz.
+        // int max = int.MinValue;
+        // int min = int.MaxValue;
+
+        // foreach (int number in numbers)
+        // {
+        //     if (number > max) max = number;
+        //     if (number < min) min = number;
+        // }
+        // // en büyük ve en küçük değerleri yazıyoruz.
+        // System.Console.WriteLine($"en küçük: {min}  En büyük: {max}" );
+        // Random rnd = new Random();
+        // int[] numbers = new int[20];
+
+
+        // Random rnd = new Random();
+        // int[] numbers = new int[20];
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     numbers[i] = rnd.Next(1, 101);
+
+
+        // }
+        // Array.Sort(numbers);
+        // System.Console.Write("Dizinin sıralanmış hali:" );
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     System.Console.Write("  " + numbers[i]);
+
+
+        // }
+
+
+        // Array.Reverse(numbers);
+        // System.Console.WriteLine("\n Dizinin  büyükten küçüğe sıralanmış hali: ");
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     System.Console.Write("  " + numbers[i]);
+
+        // }
+
+
+
+        // Random rnd = new Random();
+        // int[] numbers = new int[20];
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     numbers[i] = rnd.Next(1, 10);
+        //     System.Console.WriteLine($"Index: {i} - numbers {numbers[i]}");
+
+        // }
+        // numbers[5] = 99;
+        // System.Console.WriteLine(Array.IndexOf(numbers, 5));
+
+        // string firstName = "Alex-i de ";
+        // System.Console.WriteLine(firstName[4]);
+
+
+        // string text = "macbook ile Yazılım gelistirme ortamının hazırlanması";
+        // string[] result = text.Split("a");
+        // System.Console.WriteLine(result[0]);
+
+        // string text = "çok hava çok  güzel çok çok çok aaa aaa aaa çok ";
+        // string[] result = text.Split("çok");
+        // System.Console.WriteLine(result[0]);
+
+        // string[] names = { "Ayşen", "Umay", "Ceyda", "Begüm" };// bunlara calıs
+        // string[] newNames = new string[names.Length];
+        // Array.Copy(names, newNames, names.Length);
+        // newNames[0] = "heda";
+
+        // foreach (var name in newNames)
+
+        // {
+        //     System.Console.WriteLine(name);
+        // }
+
+        // dizide var yok kontrolü
+        // int[] numbers={3,6,7,12};
+        // bool exists=Array.Exists(numbers,n => n==6); // listenin içine n => bunula girdik sonra istediğimiz aralıkta kosul aranır
+        // System.Console.WriteLine(exists? "var" : "yok");
+        // // Dizide Belirtilen Koşula Uygun ilk Elemanı Getirmek 
+        // int [] numbers = {3,6,7,12};
+        // int result = Array.Find(numbers, n => n == 6);
+        // System.Console.WriteLine(result);
+
+        // Dizide Belirtilen Koşula Uygun ilk Elemanı Getirmek 
+        // int[] numbers = { 3, 6, 7, 12, };
+        // int[] result = Array.FindAll(numbers, x => x == 6);
+        // foreach (var n in result)
+        // {
+        //    System.Console.WriteLine(n); 
+        // }
+
+        // int [] numbers ={3,6,9};
+        // System.Console.WriteLine(numbers.Length);
+        // Array.Resize(ref numbers,4);
+        // numbers[3]=55;
+        // System.Console.WriteLine(numbers.Length);
+
+        // Dizinin Belirli Bir Bölümünü Temizlemek
+
+        // int[] numbers = { 4, 5, 6, 7, 8, 9, 12, 2, 4, 5, 12 };
+        // System.Console.WriteLine("Dizinin Örjinal Hali");
+        // foreach (var number in numbers)
+
+        // {
+        //     System.Console.WriteLine(number);
+
+        // }
+       
+     
+        
+
+
+    }
+}
---
+++ ödev/Project15_CreateAndAssignTeams/Program.cs
@@ -0,0 +1,169 @@
+﻿namespace Project15_CreateAndAssignTeams;
+
+class Program
+{
+
+    static void Greet()
+    {
+        Console.WriteLine("Merhaba Drogba");
+
+
+    }
+
+    static string Greet2(string name, bool gender)
+    {
+        if (gender)
+        {
+            return $"Merhaba {name} hanım:";
+        }
+        return $"Merhaba {name} beyfendi:) <3<3 yaşasın maskülen tayfa";
+
+    }
+
+    static double SumSqrt(double number1, double number2)
+    {
+        double toplam = number1;
+        double result = Math.Sqrt(toplam);
+        return result;
+
+    }
+
+    static int Sum1()
+
+    {
+        int a = 40;
+        int b = 50;
+        int c = a + b;
+
+        return c;
+
+    }
+
+    static void SampleNumberValue(ref int originalNumber)
+    {
+        originalNumber += 3;
+        System.Console.WriteLine($"metot içinde yazdırma sayısı : {originalNumber}");
+    }
+
+    static int Sum2(int number1, int number2)
+
+    {
+        int result = number1 + number2;
+        return result;
+    }
+
+    static void Main(string[] args) // void görüyorsan geriye değer göndermediğini anla
+    {
+
+        // Greet();
+
+        // Sum1();
+        // int x =Sum1();
+        // int y = Sum2(50,70);
+        // int z = Sum2(90,250);
+        //SumSqrt(40,50);
+        //System.Console.WriteLine(Greet2("burak", false));
+        int originalNumber = 15;
+        System.Console.WriteLine($"Metot çağırılmadan önceki orijinalNumber değeri  {originalNumber}:");
+        SampleNumberValue(ref originalNumber);
+
+        System.Console.WriteLine("metot cagırıldıktan sonra  " + originalNumber);
+
+        #region örnek
+
+
+        // *************//**1.** Kullanıcıdan alınan bir cümledeki kelime sayısını bulan bir program yazın. (`Split` metodunu araştırınız.)
+        //  System.Console.Write("Bir cümle giriniz");
+        //  string bosluk =Console.ReadLine();
+        //  string[] kelimeler=bosluk.Split(' ');
+        //  int boslukSayısı=kelimeler.Length;
+        //  System.Console.WriteLine($"girdiğiniz cümledeği kelime sayısı :{boslukSayısı}");
+
+        //**2.** Kullanıcıdan alınan bir metni tüm harfleri büyük olacak şekilde ve tüm harfleri küçük olacak şekilde alt alta tek bir Console.Write ile ekrana yazdıran bir program yazın.
+        // System.Console.WriteLine("Bir Metin Giriniz");
+        // string metin = Console.ReadLine();
+        // System.Console.WriteLine(metin.ToLower() + "  " + metin.ToUpper());
+
+
+        //************3.* *Kullanıcıdan alınan bir cümlede belirli bir kelimenin kaç kez geçtiğini bulan bir program yazın. (`Split` metodunu araştırınız.)
+        // System.Console.WriteLine("Bir cümle girinizi");
+        // string alınanveri =Console.ReadLine();
+
+        // string[] sonuc=alınanveri.Split("çok");
+        // int tekrarSayisi = sonuc.Length-1;
+        // System.Console.WriteLine(tekrarSayisi);
+
+
+        //**4.** Kullanıcıdan alınan bir cümlenin başındaki ve sonundaki boşlukları kaldıran bir program yazın. (`Trim` metotlarını araştırınız.)
+
+        // System.Console.WriteLine("notunuzu giriniz");
+        // int not = int.Parse(Console.ReadLine());
+
+        // string sonuc = "";
+        // if (not >= 54)
+
+
+        // {
+        //     if (not <= 100)
+        //     {
+        //         sonuc = "AA";
+
+        //     }
+        //     else if (not >= 89)
+        //     {
+        //         sonuc = "BA";
+
+        //     }
+        //     else if (not >= 84)
+        //     {
+        //         sonuc = "BB";
+        //     }
+
+        //     else if (not >= 79)
+        //     {
+        //         sonuc = "CB";
+        //     }
+        //     else if (not >= 69)
+        //     {
+        //         sonuc = "CC";
+        //     }
+        //     else if (not >= 59)
+        //     {
+        //         sonuc = "CD";
+
+        //     }
+        //     else
+        //     {
+        //         sonuc = "DD";
+        //     }
+
+
+        //     System.Console.WriteLine("geçerli" + "  " + sonuc);
+
+
+
+        // }
+        // else
+        // {
+        //     if (not >= 49)
+        //     {
+        //         sonuc = "fd";
+        //     }
+        //     else if (not >= 39 || not >= 1)
+        //     {
+        //         sonuc = "ff";
+        //     }
+        //     else if (not == 0)
+        //     {
+        //         sonuc = "devamsız";
+
+        //     }
+
+        //     else
+        //     {
+        //         System.Console.WriteLine("Geçerli Bir SAyı Giriniz");
+        //     }
+        #endregion
+
+    }
+}
